f1d9afcd972547ec57970f94074ac49669d49796
KK <68837552+karnking@users.noreply.github.com>
2023-06-30 10:29:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const PORT = 5000
 const path= require('path')
 const jsonServer= require('json-server')
 const server = jsonServer.create()
-const router= jsonServer.router(path.join(__dirname,'data.json'))
+const router= jsonServer.router(path.join(__dirname,'db.json'))
 const middlewares = jsonServer.defaults()
 server.use(middlewares)
 server.use(jsonServer.bodyParser)
